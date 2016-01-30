54a0b08a280411cc49a9c59ea9888a1c14aa34aa
Vamsi Voleti <vamsikirancbit@gmail.com>
2016-01-30 16:24:30 -0700

    initial commit and added files

---
+++ conditional.py
@@ -0,0 +1,14 @@
+x=7
+day = "monday"
+if x>8:
+    print("Thats how its done and it is a " +day+\
+          ".Its great and value is "+str(x)+".")
+    print("second way")
+elif day == "monday":
+    print("Its ok boss")
+else:
+    print("And this is also a way")
+    
+
+    
+    
---
+++ iteration.py
@@ -0,0 +1,5 @@
+books = ["Tale of two cities","Harry potter","Lord od the rings"]
+nums=range(100)
+for hi in nums:
+    print hi
+    
